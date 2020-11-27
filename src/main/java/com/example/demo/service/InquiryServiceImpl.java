58af181ebd19f5b3789cab41eaf00d58988bff19
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2020-11-27 22:17:34 +0900

    コードスタイルを修正

--- src/main/java/com/example/demo/service/InquiryServiceImpl.java
+++ src/main/java/com/example/demo/service/InquiryServiceImpl.java
@@ -8,34 +8,34 @@ import com.example.demo.repository.InquiryDao;
 /*
  * Add an annotation here
  */
-public class InquiryServiceImpl implements InquiryService{
+public class InquiryServiceImpl implements InquiryService {
 
 	private final InquiryDao dao;
-	
+
 	public InquiryServiceImpl(InquiryDao dao) {
 		this.dao = dao;
 	}
-	
+
 	@Override
 	public void save(Inquiry inquiry) {
 		//hands-on
 	}
-	
+
 //  This method is used in the latter chapter
 //	@Override
 //	public void update(Inquiry inquiry) {
-//		
+//
 //		//return dao.updateInquiry(inquiry);
 //		if(dao.updateInquiry(inquiry) == 0) {
 //			throw new InquiryNotFoundException("can't find the same ID");
 //		}
 //	}
-	
+
 	@Override
 	public List<Inquiry> getAll() {
-		
+
 		//hands-on
-		
+
 		return null;
 	}
 }
