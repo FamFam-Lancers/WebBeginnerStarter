97a3a27d3ad292acdac7e4fc95dd217fb5b63672
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2020-11-27 22:18:16 +0900

    不要なセミコロンを削除

--- src/main/java/com/example/demo/app/survey/SurveyForm.java
+++ src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -4,15 +4,15 @@ public class SurveyForm{
 
 	/*Add parameters(0~150) 引数を追加(0~150)*/
     private int age;
-    
+
     /*Add parameters(1~5) 引数を追加(1~5)*/
     private int satisfaction;
 
     /*Add parameters(200 characters or less) 引数を追加(200文字以内)*/
     private String comment;
-	
-	public SurveyForm() {};
-	
+
+	public SurveyForm() {}
+
     public SurveyForm(int age, int satisfaction, String comment) {
 		this.age = age;
 		this.satisfaction = satisfaction;
