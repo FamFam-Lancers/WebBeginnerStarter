90ab30b07ab18fa5a2338cccf4ad9872c6e76fe3
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2020-11-27 22:17:04 +0900

    InquiryServiceImpl -> InquiryService

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -8,51 +8,51 @@ import org.springframework.web.bind.annotation.PostMapping;
  * Add annotations here
  */
 public class InquiryController {
-	
-// 	private final InquiryServiceImpl inquiryService;
-	
-	//Add an annotation here 
-// 	public InquiryController(InquiryServiceImpl inquiryService){
+
+// 	private final InquiryService inquiryService;
+
+	//Add an annotation here
+// 	public InquiryController(InquiryService inquiryService){
 // 		this.inquiryService = inquiryService;
 // 	}
-	
+
 	@GetMapping
 	public String index(Model model) {
-		
+
 		//hands-on
-		
+
 		return "inquiry/index";
 	}
-	
+
 	@GetMapping("/form")
 	public String form(/*Add parameters. */) {
-		
+
 		return "inquiry/form";
 	}
-	
+
 	@PostMapping("/form")
 	public String formGoBack(InquiryForm inquiryForm, Model model) {
 		model.addAttribute("title", "InquiryForm");
 		return "inquiry/form";
 	}
-	
-	
+
+
 	@PostMapping("/confirm")
 	public String confirm(/*Add parameters. */) {
 
 		//hands-on
-		
+
 		return "inquiry/confirm";
 	}
-	
+
 	@PostMapping("/complete")
 	public String complete(/*Add parameters. */) {
-		
+
 		//hands-on
-		
+
 		//redirect
-		
+
 		return "";
 	}
-	
+
 }
