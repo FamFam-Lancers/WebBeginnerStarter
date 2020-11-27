5e2437287cc790ed80c7ff73308c2d8ff2c2daa1
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2020-11-27 22:17:56 +0900

    @Repositoryを削除

--- src/main/java/com/example/demo/repository/SurveyDao.java
+++ src/main/java/com/example/demo/repository/SurveyDao.java
@@ -6,11 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Survey;
 
-@Repository
 public interface SurveyDao {
-	
+
 	void insertSurvey(Survey survey);
-	
+
 	List<Survey> getAll();
-	
+
 }
